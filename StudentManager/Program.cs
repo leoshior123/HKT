804e16eff73203502ea52cef65d2c2b8cf138f7d
LeoShior <LeoShior@LEOSHIOR>
2019-11-15 11:21:42 +0700

    nop bai tap 2  tran dinh hoang thien, tran dang khoa

---
+++ StudentManager/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager
+{
+    class Program
+    {
+       static Student[] studentList;
+        static void Main(string[] args)
+        {
+            //nhap tong so sinh vien
+            int numOfStudent;
+            
+            try
+            {
+                Console.Write("Num of Student: ");
+                numOfStudent = int.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Invalid Input Number Of Student!!!(0)",ex.Message);
+                Console.ReadKey();
+                return;
+                    
+            }
+            //tao danh sach sinh vien
+            InputStudentList(numOfStudent);
+            OutputStudentList();
+            Console.ReadKey();
+
+        }
+        /// <summary>
+        /// ham xuat ds sinh vien
+        /// </summary>
+        private static void OutputStudentList()
+        {
+            foreach (var student in studentList)
+            {
+                Console.WriteLine("Student ID = [0]", student.StudentID);
+                Console.WriteLine("Student FullName = [0]", student.FullName);
+                Console.WriteLine("Student Mark = [0]", student.Mark);
+                Console.WriteLine("Student Falculty = [0]", student.Falculty);
+            }
+        }
+
+        private static void InputStudentList(int numOfStudent)
+        {
+            //tao mang luu danh sach sinh vien
+
+            studentList =new Student[numOfStudent];
+            Student student;
+            for (int i = 0; i < numOfStudent; i++)
+            {
+                student = new Student();
+                Console.Write("Student ID = ");
+                student.StudentID = int.Parse(Console.ReadLine());
+                Console.Write("Student FullName = ");
+                student.FullName = Console.ReadLine();
+                Console.Write("Student Mark = ");
+                student.Mark = float.Parse(Console.ReadLine());
+                Console.Write("Student Falculty = ");
+                student.Falculty = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ StudentManager/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager
+{
+    class Student
+    {
+        //crtl+c+k: coment
+        //crtl+k+u:uncoment
+        //crtl+k+d: format code
+        public int StudentID { get; set; }
+        public string FullName { get; set; }
+        public float Mark  { get; set; }
+        public string Falculty { get; set; }
+        public Student()
+        {
+
+        }
+        public Student(int studentID,string fullName,float mark,string falculty)
+        {
+            StudentID = studentID;
+            FullName = fullName;
+            Mark = mark;
+            Falculty = falculty;
+
+        }
+    }
+}
