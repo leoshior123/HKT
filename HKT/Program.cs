de72cedc31a0157d298e7c292d8ea6d575b34645
LeoShior <LeoShior@LEOSHIOR>
2019-11-15 10:08:27 +0700

    nhom HKT: tran dinh hoang thien , tran dang khoa,

---
+++ HKT/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HKT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BAI TAP 2 - NHOM HKT
+            //khoi tao project
+        }
+    }
+}
